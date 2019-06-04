526948aaa833565a2190d91901cdaa443988e018
Jason Macklin <jasonmacklin@Jasons-MacBook-Pro.local>
2019-06-04 00:43:08 -0400

    initial

---
+++ ch02/02_04/start/react-is-fun/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var style = {
+
+    backgroundColor: 'orange',
+    color:'white',
+    fontFamily: 'Arial'
+}
+ 
+
+    
+
+
+
+
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World<h1>
+        <p>We're glad youre here</p>    
+
+</div>
+    
+    
+    
+    title,
+    document.getElementById('root')
+)
